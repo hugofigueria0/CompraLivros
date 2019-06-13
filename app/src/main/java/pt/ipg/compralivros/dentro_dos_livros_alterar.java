35ec363c6065ac18a456c11bdda613af67f33760
hugofigueira0 <hugofigueira0@gmail.com>
2019-06-13 14:27:24 +0100

    Acrescentação da funcionalidade de Alterar na tabela Livros

--- app/src/main/java/pt/ipg/compralivros/dentro_dos_livros_alterar.java
+++ app/src/main/java/pt/ipg/compralivros/dentro_dos_livros_alterar.java
@@ -1,18 +1,38 @@
 package pt.ipg.compralivros;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class dentro_dos_livros_alterar extends AppCompatActivity {
+public class dentro_dos_livros_alterar extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<Cursor>{
+    private static final int ID_CURSO_LOADER_CATEGORIAS = 0;
+
+    private EditText editTextTitulo;
+    private EditText editTextCategoria;
+    private EditText editTextData;
+
+    private Livro livro = null;
+
+
+
+    private Uri enderecoLivroEditar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,49 @@ public class dentro_dos_livros_alterar extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editTextTitulo = (EditText) findViewById(R.id.editTextTitulo);
+        editTextCategoria = (EditText) findViewById(R.id.editTextCategoria);
+        editTextData = (EditText) findViewById(R.id.editTextData);
+
+        getSupportLoaderManager().initLoader(ID_CURSO_LOADER_CATEGORIAS, null, this);
+
+        Intent intent = getIntent();
+
+        long idLivro = intent.getLongExtra(DentroDosLivros.ID_LIVRO, -1);
+
+        if (idLivro == -1) {
+            Toast.makeText(this, "Erro: não foi possível ler o livro", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        enderecoLivroEditar = Uri.withAppendedPath(LivrosContentProvider.ENDERECO_LIVROS, String.valueOf(idLivro));
+
+        Cursor cursor = getContentResolver().query(enderecoLivroEditar, BdTableLivros.TODAS_COLUNAS, null, null, null);
+
+        if (!cursor.moveToNext()) {
+            Toast.makeText(this, "Erro: não foi possível ler o livro", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        livro = Livro.fromCursor(cursor);
+
+        editTextTitulo.setText(livro.getTitulo());
+        editTextCategoria.setText(livro.getCategoria());
+        editTextData.setText(String.valueOf(livro.getData()));
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        getSupportLoaderManager().restartLoader(ID_CURSO_LOADER_CATEGORIAS, null, this);
+
+        super.onResume();
     }
 
     public void Cancelar(View view){
@@ -29,6 +92,7 @@ public class dentro_dos_livros_alterar extends AppCompatActivity {
     }
 
     public void Alterar(View view){
+
         ValidarEscrita();
         Toast.makeText(this, "Foi alterado", Toast.LENGTH_LONG).show();
 
@@ -36,41 +100,71 @@ public class dentro_dos_livros_alterar extends AppCompatActivity {
 
     public void ValidarEscrita(){
 
-        EditText editeditTextTitulo = (EditText) findViewById(R.id.editTextTitulo);
-        String Livro = editeditTextTitulo.getText().toString();
+        String titulo = editTextTitulo.getText().toString();
 
-        EditText editeditTextCategoria = (EditText) findViewById(R.id.editTextCategoria);
-        String Genero = editeditTextCategoria.getText().toString();
-
-        EditText editeditTextData = (EditText) findViewById(R.id.editTextData);
-        String Data = editeditTextData.getText().toString();
-
-        if(Livro.trim().length() == 0){
-
-            editeditTextTitulo.setError("Titulo Invalido");
-            editeditTextTitulo.requestFocus();
+        if (titulo.trim().isEmpty()) {
+            editTextTitulo.setError("Preencha o titulo");
             return;
-
-
         }
-        if(Genero.trim().length() == 0){
 
-            editeditTextCategoria.setError("Categoria Invalida");
-            editeditTextCategoria.requestFocus();
+        String data = editTextData.getText().toString();
+
+        if (titulo.trim().isEmpty()) {
+            editTextData.setError("Preencha o titulo");
             return;
-
         }
-        if(Data.trim().length() == 0){
 
-            editeditTextData.setError("Data Invalida");
-            editeditTextData.requestFocus();
+        String Categoria = editTextCategoria.getText().toString();
+
+        if (titulo.trim().isEmpty()) {
+            editTextCategoria.setError("Preencha o titulo");
             return;
-
         }
 
 
-        finish();
+        livro.setTitulo(titulo);
+        livro.setCategoria(Categoria);
+        livro.setData(data);
+
+        try {
+            getContentResolver().update(enderecoLivroEditar, livro.getContentValues(), null, null);
+
+            Toast.makeText(this, "livro alterado", Toast.LENGTH_SHORT).show();
+            finish();
+        } catch (Exception e) {
+            Snackbar.make(
+                    editTextTitulo,
+                    "Erro ao alterar",
+                    Snackbar.LENGTH_LONG)
+                    .show();
+
+            e.printStackTrace();
+        }
 
     }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        androidx.loader.content.CursorLoader cursorLoader = new androidx.loader.content.CursorLoader(this, LivrosContentProvider.ENDERECO_LIVROS, BdTableLivros.TODAS_COLUNAS, null, null, BdTableLivros.CAMPO_TITULO
+        );
+
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+
+
+    }
+
 
 }
